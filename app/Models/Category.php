9adc1320cf9457972c6c6a1b4327a2df947a8b66
Asmanuha16 <amouretfleu@gmail.com>
2025-02-03 16:08:54 +0700

    review

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -7,7 +7,8 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Category extends Model
 {
-    use HasFactory; 
-    protected $fillable=['name'];
+    protected $fillable=[
+        'name'
+    ];
 
  }
